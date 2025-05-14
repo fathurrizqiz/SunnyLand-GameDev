9a5e73299805a50fc49cb4d7b4effb0d53e41cd9
Fathur Rizqi Putra Pratama <ezzdarkrap@gmail.com>
2025-05-14 22:44:07 +0700

    SunnyLand

---
+++ Assets/Script/test.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    //public GameObject player;
+    //private void awake()
+    //{
+    //    player.SetActive(true);
+    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("start");
+    }
+}
